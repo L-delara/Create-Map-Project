a488a1f04dc381a9fe36f0f0314ceb0457313515
Ldelara <ligia.delara15@gmail.com>
2023-03-29 19:40:33 -0400

    Label selectors and find current position

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-// What events will your application need?
+// What events will your application need? -- select and submit map options
 
-// What APIs will you need and in what order?
+// What APIs will you need and in what order? -- geolocation and local businesses(foursquare)
 
-// How will you obtain the user's location?
+// How will you obtain the user's location? -- leaflet api
 
 // How will you add the user's location to the map?
 
-// How will you get the selected location from the user?
+// How will you get the selected location from the user? -- on load, get location
 
 // How will you add that information to the map?
+
+function findPosition(position) {
+  console.log(position.coords.latitude);
+  console.log(position.coords.longitude);
+}
+
+navigator.geolocation.getCurrentPosition(findPosition);
