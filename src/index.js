c70228e3939397344d04e9441e0ed0b659996c85
Ldelara <ligia.delara15@gmail.com>
2023-03-29 18:38:46 -0400

    saved functionality notes to js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// What events will your application need?
+
+// What APIs will you need and in what order?
+
+// How will you obtain the user's location?
+
+// How will you add the user's location to the map?
+
+// How will you get the selected location from the user?
+
+// How will you add that information to the map?
