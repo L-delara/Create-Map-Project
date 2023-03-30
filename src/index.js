f567a222d04eb23563540da80b239d6a22041a22
Ldelara <ligia.delara15@gmail.com>
2023-03-29 21:12:26 -0400

    map not coming up
    reviewed docs and video, can't find where it's not showing

--- src/index.js
+++ src/index.js
@@ -10,9 +10,30 @@
 
 // How will you add that information to the map?
 
-function findPosition(position) {
-  console.log(position.coords.latitude);
-  console.log(position.coords.longitude);
-}
+//create the map
+const myMap = L.Map("map", {
+  center: [35.77, -78.79],
+  zoom: 15,
+});
 
-navigator.geolocation.getCurrentPosition(findPosition);
+//streetmap tiles
+L.tileLayer(
+  "https://tile.thunderforest.com/neighbourhood/{z}/{x}/{y}.png?apikey=41061f58f4b647bb94dad0063b961f79",
+  {
+    attribution:
+      '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    apikey: "41061f58f4b647bb94dad0063b961f79",
+    maxZoom: 18,
+  }
+);
+
+//Geolocation pin
+const marker = L.marker([35.77, -78.79]);
+marker.addTo(myMap);
+
+// function findPosition(position) {
+//   console.log(position.coords.latitude);
+//   console.log(position.coords.longitude);
+// }
+
+// navigator.geolocation.getCurrentPosition(findPosition);
